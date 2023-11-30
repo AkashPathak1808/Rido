4c6e30854a90e3ebdade5e91f6a3783e68de191d
unknown <shubhampandey5600@gmail.com>
2023-11-30 12:03:17 +0530

    aboutUs Updates

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -7,12 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AboutUsComponent {
 
-  imagepath="assets/aboutUsImg/image1.svg";
-  imagepath1="assets/aboutUsImg/Ellipse 6.svg";
-  imagepath2="assets/aboutUsImg/image2.svg";
-  imagepath3="assets/aboutUsImg/Ellipse 7.svg";
-  imagepath4="assets/aboutUsImg/Rectangle.svg";
-  imagepath5="assets/aboutUsImg/image3.svg";
-
-
+  imagepath="assets/aboutUsImg/image1.svg";;
+  imagepath1="assets/aboutUsImg/image2.svg";
+  imagepath3="assets/aboutUsImg/image4.png";
 }
